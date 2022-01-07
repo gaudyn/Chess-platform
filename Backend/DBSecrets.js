96b5c1f5a6728ca3c804e45dece0bea2168b7336
gniewko717 <ggaudyn00@gmail.com>
2022-01-07 13:23:21 +0100

    Add DB secrets

---
+++ Backend/DBSecrets.js
@@ -0,0 +1,10 @@
+const { Client } = require('pg');
+
+// Do uzupełnienia 
+const client = new Client({
+    user: '',
+    host: 'localhost',
+    database: '',
+    password: '', 
+    port: '5432'
+});
