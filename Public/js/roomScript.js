80d5a9ce9a76f4e46de3013e84f67607ef3a6641
gniewko717 <ggaudyn00@gmail.com>
2022-01-18 13:56:18 +0100

    Add room audience

--- Public/js/roomScript.js
+++ Public/js/roomScript.js
@@ -1,20 +1,61 @@
 console.log('Client connected to server!');
 
-//import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
-//import {io} from 'socket.io-client';
-function createSocket(username) {
-    var socket = io("http://localhost:3000", {autoConnect: false});
-    socket.auth = {username, roomId};
-    socket.connect();
+var whitePlayer;
+var blackPlayer;
+var audience;
 
-    socket.on('message', (msg, user) => {
-        console.log(user+": "+msg);
-    });
 
-    return socket;
+function ConnectionHandler(username) {
+    this.socket = (function (){
+        var socket = io("http://localhost:3000", {autoConnect: false});
+        socket.auth = {username, roomId};
+        socket.connect();
+
+        socket.on('fillRoom', (room) => {
+            console.log('Fill room with '+ room.toString());
+            whitePlayer.innerHTML = room.whitePlayer;
+            blackPlayer.innerHTML = room.blackPlayer;
+            audience.innerHTML = room.audience.toString();
+        })
+
+        socket.on('claimPlace', (place, player) => {
+            if (place == 'white'){
+                whitePlayer.innerHTML = player
+            } else if (place == 'black') {
+                blackPlayer.innerHTML = player
+            } else {
+                audience.innerHTML = player
+            }
+        });
+
+        socket.on('move', (move, player) => {
+            // Update gameboard with new move
+        });
+
+        return socket;
+    })();
+    
+    // Managing position in the room
+    this.claimWhitePlace = function(){
+        this.socket.emit('claimPlace', 'white');
+    }
+
+    this.claimBlackPlace = function(){
+        this.socket.emit('claimPlace', 'black');
+    }
+
+    this.unclaimPlace = function(){
+        this.socket.emit('claimPlace', 'watch');
+    }
+
+    // Gameplay
+    this.makeMove = function(move){
+        this.socket.emit('move', move);
+    }
 }
 
-function sayHello(socket) {
-    socket.emit('message', 'Hello!');
-}
-
+window.addEventListener('load', (window, event) => {
+    whitePlayer = document.getElementById('whitePlayer');
+    blackPlayer = document.getElementById('blackPlayer');
+    audience = document.getElementById('audience');
+});
--- app.js
+++ app.js
@@ -16,16 +16,39 @@ app.get('/room/:roomId', (req, res) => {
     res.render('room.ejs', {roomId: req.params.roomId});
 });
 
+var rooms = [];
+
 io.on('connection', (socket) => {
     let userData = socket.handshake.auth;
+    let roomId = userData.roomId.toString();
 
-    console.log('Gracz przyłączony: '+ userData.username);
-    socket.join(userData.roomId.toString());
+    console.log('Gracz '+userData.username +' przyłączony do pokoju '+ roomId);
+    if(!rooms[roomId]){
+        rooms[roomId] = {
+            whitePlayer: "",
+            blackPlayer: "",
+            audience: [userData.username]
+        }
+    } else {
+        rooms[roomId].audience.push(userData.username);
+    }
+    
+    socket.join(roomId);
 
-    socket.on('message', (msg) => {
-        console.log(userData.username+"@"+userData.roomId + ": " + msg);
-        io.to(userData.roomId.toString()).emit('message', msg, userData.username);
+    socket.on('disconnecting', (reason) => {
+        for(const room of socket.rooms) {
+            if(room == socket.id) continue;
+            let index = rooms[room].audience.indexOf(userData.username)
+            rooms[room].audience.splice(index, 1);
+            socket.to(roomId).emit('fillRoom', rooms[roomId]);
+        }
+        console.log('Gracz '+userData.username+' został odłączony');
     });
+
+    io.to(roomId).emit('fillRoom', rooms[roomId]);
+    console.log(socket.id);
+
+    
 })
 
 
