88a7693cdd83a854573ba6c59de984da3ea8eed5
Dawid-Sroka <dawid.sroka.s@gmail.com>
2022-01-26 20:27:18 +0100

    improve cookie recognition

--- authorize.js
+++ authorize.js
@@ -1,16 +1,20 @@
 /**
-*
+* Redirects to the mainpage every user without a cookie
 * @param {http.IncomingMessage} req
 * @param {http.ServerResponse} res
 * @param {*} next
 */
 function authorize(req, res, next) {
-    if (req.signedCookies.user) {
-        req.user = req.signedCookies.user;
-        next();
+    if (req.signedCookies.cookie) {
+        var userCookie = JSON.parse(req.signedCookies.cookie);
+        if (userCookie.userType == 'loggedIn' | userCookie.userType == 'noAccount') {
+            // jwt token
+            next();
+        } else {
+        res.redirect('./waitroom');
+        }
     } else {
-        //res.redirect('/');
-        next();
+        res.redirect('/');
     }
 }
 
