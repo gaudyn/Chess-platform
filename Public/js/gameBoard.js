56498b42266e3519f63aabb1f38918caf0703cca
gniewko717 <ggaudyn00@gmail.com>
2022-01-27 11:23:19 +0100

    Beautify

--- Public/js/gameBoard.js
+++ Public/js/gameBoard.js
@@ -1,48 +1,54 @@
 function Square(props) {
-  if(props.color === 'white'){
-    return (
-    <div className="square" key={props.key} style={{
-      backgroundColor: 'white'}}>
-      <span>{props.value}</span>
-    </div>);
-  } else {
-    return (
-    <div className="square" key={props.key} style={{
-      backgroundColor: 'gray'}}>
-      <span>{props.value}</span>
-    </div>);
-  }
+    if(props.color === 'white'){
+        return (
+            <div className="square" key={props.key} style={{
+            backgroundColor: 'white'}}>
+            <span>{props.value}</span>
+            </div>
+        );
+    } else {
+        return (
+            <div className="square" key={props.key} style={{
+            backgroundColor: 'gray'}}>
+            <span>{props.value}</span>
+            </div>
+        );
+    }
 }
 
 class Board extends React.Component {
-  render() {
-    const pieces = [
-      ['♜','♞','♝','♚','♛','♝','♞','♜'],
-      ['♟','♟','♟','♟','♟','♟','♟','♟'],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
-      ['♙','♙','♙','♙','♙','♙','♙','♙'],
-      ['♖','♘','♗','♔','♕','♗','♘','♖']
-    ]
-    return(
-      [...Array(8).keys()].map((y) => {
-        return (<div className="board-row" key={y}>
-        {
-          [...Array(8).keys()].map((x) => {
-            return (Square({color: (x+y)%2 ? 'white' : 'black', value: pieces[y][x], key:x}))
-          })
-        }
-        </div>)
-      })
-    )
-  }
+    constructor() {
+        this.pieces = [
+            ['♜','♞','♝','♚','♛','♝','♞','♜'],
+            ['♟','♟','♟','♟','♟','♟','♟','♟'],
+            [' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ',' '],
+            ['♙','♙','♙','♙','♙','♙','♙','♙'],
+            ['♖','♘','♗','♔','♕','♗','♘','♖']
+          ]
+    }
+    render() {
+        return(
+        [...Array(8).keys()].map((y) => {
+            return (
+            <div className="board-row" key={y}>
+                {
+                [...Array(8).keys()].map((x) => {
+                    return (Square({color: (x+y)%2 ? 'white' : 'black', value: this.pieces[y][x], key:x}))
+                })
+                }
+            </div>
+            );
+        })
+        )
+    }
 }
 
 // ============
 
 ReactDOM.render(
-  <Board />,
-  document.getElementById('gameBoard')
+    <Board />,
+    document.getElementById('gameBoard')
 );
